0f7150dfe879b46e7918cedceb311110b56a7ee0
Kenny Root <kenny@the-b.org>
2015-09-15 07:38:47 -0700

    Add reply and tests

--- simplesocks/src/main/java/org/connectbot/simplesocks/Socks5Server.java
+++ simplesocks/src/main/java/org/connectbot/simplesocks/Socks5Server.java
@@ -1,13 +1,80 @@
 package org.connectbot.simplesocks;
 
 import java.io.*;
+import java.net.InetAddress;
+import java.nio.charset.Charset;
 
 /**
  * Created by kroot on 9/15/15.
  */
 public class Socks5Server {
+    /** Address type that indicates the request is IPv4. */
+    private static final int ATYPE_IPV4 = 0x01;
+
+    /** Address type that indicates the request is IPv6. */
+    private static final int ATYPE_DNS = 0x03;
+
+    /** Address type that indicates the request is IPv6. */
+    private static final int ATYPE_IPV6 = 0x04;
+
     private final DataInputStream in;
     private final DataOutputStream out;
+
+    public enum Command {
+        CONNECT(0x01),
+        BIND(0x02);
+
+        public static Command fromCommandNumber(int commandNumber) {
+            if (commandNumber == Command.CONNECT.commandNumber()) {
+                return Command.CONNECT;
+            } else if (commandNumber == Command.BIND.commandNumber()) {
+                return Command.BIND;
+            } else {
+                return null;
+            }
+        }
+
+        private final int commandNumber;
+
+        Command(int commandNumber) {
+            this.commandNumber = commandNumber;
+        }
+
+        public int commandNumber() {
+            return commandNumber;
+        }
+    }
+
+    public enum ResponseCode {
+        SUCCESS((byte) 0x00),
+        GENERAL_FAILURE((byte) 0x01),
+        RULESET_DENIED((byte) 0x02),
+        NETWORK_UNREACHABLE((byte) 0x03),
+        HOST_UNREACHABLE((byte) 0x04),
+        CONNECTION_REFUSED((byte) 0x05),
+        TTL_EXPIRED((byte) 0x06),
+        COMMAND_NOT_SUPPORTED((byte) 0x07),
+        ADDRESS_TYPE_NOT_SUPPORTED((byte) 0x08);
+
+        private final byte code;
+
+        ResponseCode(byte code) {
+            this.code = code;
+        }
+
+        public byte getCode() {
+            return code;
+        }
+    }
+
+    /** The command the request is referring to. */
+    private Command command;
+
+    /** Address requested when the {@link Command} was given. */
+    private InetAddress address;
+
+    /** The port requested when the {@link Command} was given. */
+    private int port = -1;
 
     public Socks5Server(InputStream in, OutputStream out) {
         this.in = new DataInputStream(in);
@@ -15,17 +82,15 @@ public class Socks5Server {
     }
 
     public boolean acceptAuthentication() throws IOException {
-        if (in.read() != 0x05) {
-            throw new IOException("Unsupported protocol");
-        }
+        checkProtocolVersion();
 
         int numMethods = in.read();
         byte[] methods = new byte[numMethods];
         in.readFully(methods);
 
         boolean success = false;
-        for (int i = 0; i < methods.length; i++) {
-            if (methods[i] == 0x00) {
+        for (byte method : methods) {
+            if (method == 0x00) {
                 success = true;
                 break;
             }
@@ -40,5 +105,72 @@ public class Socks5Server {
         }
         out.write(reply);
         return success;
+    }
+
+    private void checkProtocolVersion() throws IOException {
+        if (in.read() != 0x05) {
+            throw new IOException("Unsupported protocol");
+        }
+    }
+
+    public boolean readRequest() throws IOException {
+        checkProtocolVersion();
+
+        boolean correct = true;
+
+        command = Command.fromCommandNumber(in.read());
+        if (command == null) {
+            correct = false;
+        }
+
+        if (in.read() != 0x00) {
+            correct = false;
+        }
+
+        int atype = in.read();
+        if (atype == ATYPE_IPV4) {
+            byte[] addressBytes = new byte[4];
+            in.readFully(addressBytes);
+            address = InetAddress.getByAddress(addressBytes);
+        } else if (atype == ATYPE_DNS) {
+            int hostNameLength = in.read();
+            byte[] hostName = new byte[hostNameLength];
+            in.readFully(hostName);
+            address = InetAddress.getByName(new String(hostName, Charset.forName("US-ASCII")));
+        } else if (atype == ATYPE_IPV6) {
+            byte[] addressBytes = new byte[16];
+            in.readFully(addressBytes);
+            address = InetAddress.getByAddress(addressBytes);
+        } else {
+            correct = false;
+        }
+
+        port = in.read() << 8 | in.read();
+
+        return correct;
+    }
+
+    public void sendReply(ResponseCode response) throws IOException {
+        byte[] responseBytes = new byte[]{
+                (byte) 0x05, /* version */
+                response.getCode(),
+                (byte) 0x00, /* reserved */
+                (byte) 0x01, /* Address type: IPv4 */
+                (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, /* INADDR_ANY */
+                (byte) 0x00, (byte) 0x00, /* port */
+        };
+        out.write(responseBytes);
+    }
+
+    public Command getCommand() {
+        return command;
+    }
+
+    public InetAddress getAddress() {
+        return address;
+    }
+
+    public int getPort() {
+        return port;
     }
 }
--- simplesocks/src/test/java/org/connectbot/simplesocks/Socks5ServerTest.java
+++ simplesocks/src/test/java/org/connectbot/simplesocks/Socks5ServerTest.java
@@ -15,7 +15,7 @@ import static org.junit.Assert.*;
 public class Socks5ServerTest {
     @Test(expected = IOException.class)
     public void testAuthentication_NotSocks5_Failure() throws Exception {
-        InputStream in = wrapBytes(new byte[] { 0x01, 0x01, 0x00 });
+        ByteArrayInputStream in = wrapBytes((byte) 0x01, (byte) 0x01, (byte) 0x00);
         ByteArrayOutputStream out = new ByteArrayOutputStream();
         Socks5Server server = new Socks5Server(in, out);
         server.acceptAuthentication();
@@ -23,20 +23,25 @@ public class Socks5ServerTest {
 
     @Test
     public void testAuthentication_SingleMethod_Success() throws Exception {
-        InputStream in = wrapBytes(new byte[] { 0x05, 0x01, 0x00 });
+        getAuthenticatedSocks5Server((byte) 0x05, (byte) 0x01, (byte) 0x00);
+    }
+
+    private Socks5Server getAuthenticatedSocks5Server(byte... input) throws IOException {
+        InputStream in = wrapBytes(input);
         ByteArrayOutputStream out = new ByteArrayOutputStream();
         Socks5Server server = new Socks5Server(in, out);
         assertTrue(server.acceptAuthentication());
-        assertArrayEquals(new byte[] { 0x05, 0x00 }, out.toByteArray());
+        assertArrayEquals(new byte[]{0x05, 0x00}, out.toByteArray());
+        return server;
     }
 
     @Test
     public void testAuthentication_MultipleMethods_Success() throws Exception {
-        InputStream in = wrapBytes(new byte[] { 0x05, 0x03, 0x02, 0x01, 0x00 });
+        InputStream in = wrapBytes((byte) 0x05, (byte) 0x03, (byte) 0x02, (byte) 0x01, (byte) 0x00);
         ByteArrayOutputStream out = new ByteArrayOutputStream();
         Socks5Server server = new Socks5Server(in, out);
         assertTrue(server.acceptAuthentication());
-        assertArrayEquals(new byte[] { 0x05, 0x00 }, out.toByteArray());
+        assertArrayEquals(new byte[]{0x05, 0x00}, out.toByteArray());
     }
 
     @Test
@@ -48,7 +53,116 @@ public class Socks5ServerTest {
         assertArrayEquals(new byte[] { 0x05, (byte) 0xFF }, out.toByteArray());
     }
 
-    private InputStream wrapBytes(byte[] bytes) {
+    @Test(expected = IOException.class)
+    public void testReadRequest_InvalidProtocol_Failure() throws Exception {
+        Socks5Server server = getAuthenticatedSocks5Server((byte) 0x05, (byte) 0x01, (byte) 0x00,
+                (byte) 0x01, (byte) 0x01, (byte) 0x00, (byte) 0x01, (byte) 0xC0, (byte) 0xA8, (byte) 0x01, (byte) 0x01);
+        assertTrue(server.readRequest());
+        assertArrayEquals(new byte[]{(byte) 0xC0, (byte) 0xA8, (byte) 0x01, (byte) 0x01}, server.getAddress().getAddress());
+    }
+
+    @Test(expected = IOException.class)
+    public void testReadRequest_InvalidReserved_Failure() throws Exception {
+        Socks5Server server = getAuthenticatedSocks5Server((byte) 0x05, (byte) 0x01, (byte) 0x00,
+                (byte) 0x01, (byte) 0x01, (byte) 0x10, (byte) 0x01, (byte) 0xC0, (byte) 0xA8, (byte) 0x01, (byte) 0x01);
+        assertTrue(server.readRequest());
+        assertArrayEquals(new byte[]{(byte) 0xC0, (byte) 0xA8, (byte) 0x01, (byte) 0x01}, server.getAddress().getAddress());
+    }
+
+    @Test
+    public void testReadRequest_UnknownCommand_Failure() throws Exception {
+        Socks5Server server = getAuthenticatedSocks5Server((byte) 0x05, (byte) 0x01, (byte) 0x00,
+                (byte) 0x05, (byte) 0xC0, (byte) 0x00, (byte) 0x01, (byte) 0xC0, (byte) 0xA8, (byte) 0x01, (byte) 0x01);
+        assertFalse(server.readRequest());
+        assertEquals(null, server.getCommand());
+    }
+
+    @Test
+    public void testReadRequest_ConnectIPv4_Success() throws Exception {
+        Socks5Server server = getAuthenticatedSocks5Server((byte) 0x05, (byte) 0x01, (byte) 0x00,
+                (byte) 0x05, (byte) 0x01, (byte) 0x00, (byte) 0x01, (byte) 0xC0, (byte) 0xA8, (byte) 0x01, (byte) 0x01,
+                (byte) 0x07, (byte) 0xD1);
+        assertTrue(server.readRequest());
+        assertEquals(Socks5Server.Command.CONNECT, server.getCommand());
+        assertArrayEquals(new byte[]{(byte) 0xC0, (byte) 0xA8, (byte) 0x01, (byte) 0x01}, server.getAddress().getAddress());
+        assertEquals(2001, server.getPort());
+    }
+
+    @Test
+    public void testReadRequest_ConnectDNS_Success() throws Exception {
+        Socks5Server server = getAuthenticatedSocks5Server((byte) 0x05, (byte) 0x01, (byte) 0x00,
+                (byte) 0x05, (byte) 0x01, (byte) 0x00, (byte) 0x03, (byte) 0x0B, (byte) 'e', (byte) 'x', (byte) 'a',
+                (byte) 'm', (byte) 'p', (byte) 'l', (byte) 'e', (byte) '.', (byte) 'c', (byte) 'o', (byte) 'm',
+                (byte) 0x22, (byte) 0xB8);
+        assertTrue(server.readRequest());
+        assertEquals(Socks5Server.Command.CONNECT, server.getCommand());
+        assertEquals("example.com", server.getAddress().getHostName());
+        assertEquals(8888, server.getPort());
+    }
+
+    @Test
+    public void testReadRequest_ConnectIPv6_Success() throws Exception {
+        Socks5Server server = getAuthenticatedSocks5Server((byte) 0x05, (byte) 0x01, (byte) 0x00,
+                (byte) 0x05, (byte) 0x01, (byte) 0x00, (byte) 0x04,
+                /* IPv6 address; 32 bits on a row. */
+                (byte) 0x20, (byte) 0x01, (byte) 0x0D, (byte) 0xB8,
+                (byte) 0xA5, (byte) 0x5A, (byte) 0xFF, (byte) 0x02,
+                (byte) 0xCC, (byte) 0xAA, (byte) 0x01, (byte) 0x10,
+                (byte) 0x00, (byte) 0x00, (byte) 0xD0, (byte) 0x0D,
+                /* port */
+                (byte) 0x3F, (byte) 0xAE);
+        assertTrue(server.readRequest());
+        assertEquals(Socks5Server.Command.CONNECT, server.getCommand());
+        assertArrayEquals(new byte[]{(byte) 0x20, (byte) 0x01, (byte) 0x0D, (byte) 0xB8,
+                (byte) 0xA5, (byte) 0x5A, (byte) 0xFF, (byte) 0x02,
+                (byte) 0xCC, (byte) 0xAA, (byte) 0x01, (byte) 0x10,
+                (byte) 0x00, (byte) 0x00, (byte) 0xD0, (byte) 0x0D}, server.getAddress().getAddress());
+        assertEquals(16302, server.getPort());
+    }
+
+    @Test
+    public void testReadRequest_BindIPv4_Success() throws Exception {
+        Socks5Server server = getAuthenticatedSocks5Server((byte) 0x05, (byte) 0x01, (byte) 0x00,
+                (byte) 0x05, (byte) 0x02, (byte) 0x00, (byte) 0x01, (byte) 0xC0, (byte) 0xA8, (byte) 0x01, (byte) 0x01,
+                /* port */
+                (byte) 0x04, (byte) 0x38);
+        assertTrue(server.readRequest());
+        assertEquals(Socks5Server.Command.BIND, server.getCommand());
+        assertArrayEquals(new byte[]{(byte) 0xC0, (byte) 0xA8, (byte) 0x01, (byte) 0x01}, server.getAddress().getAddress());
+        assertEquals(1080, server.getPort());
+    }
+
+    @Test
+    public void testReadRequest_InvalidReservedByte_Failure() throws Exception {
+        Socks5Server server = getAuthenticatedSocks5Server((byte) 0x05, (byte) 0x01, (byte) 0x00,
+                (byte) 0x05, (byte) 0x01, (byte) 0x01, (byte) 0x01, (byte) 0xC0, (byte) 0xA8, (byte) 0x01, (byte) 0x01);
+        assertFalse(server.readRequest());
+    }
+
+    @Test
+    public void testReadRequest_ConnectUnknownProtocol_Failure() throws Exception {
+        Socks5Server server = getAuthenticatedSocks5Server((byte) 0x05, (byte) 0x01, (byte) 0x00,
+                (byte) 0x05, (byte) 0x01, (byte) 0x00, (byte) 0xA8, (byte) 0xC0, (byte) 0xA8, (byte) 0x01, (byte) 0x01);
+        assertFalse(server.readRequest());
+    }
+
+    @Test
+    public void testSendReply_Success() throws IOException {
+        InputStream in = new ByteArrayInputStream(new byte[0]);
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        Socks5Server server = new Socks5Server(in, out);
+        server.sendReply(Socks5Server.ResponseCode.SUCCESS);
+        assertArrayEquals(new byte[] {
+                (byte) 0x05, /* version */
+                (byte) 0x00, /* code */
+                (byte) 0x00, /* reserved */
+                (byte) 0x01, /* atype (IPv4) */
+                (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, /* address */
+                (byte) 0x00, (byte) 0x00}, /* port */
+                out.toByteArray());
+    }
+
+    private ByteArrayInputStream wrapBytes(byte... bytes) {
         return new ByteArrayInputStream(bytes);
     }
 }
