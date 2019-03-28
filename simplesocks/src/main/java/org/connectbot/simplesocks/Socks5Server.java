8cef2ec32b50775623f823700323ff4168026545
Kenny Root <kenny@the-b.org>
2019-03-28 23:39:07 +0900

    Fix imports to not use wildcard

--- simplesocks/src/main/java/org/connectbot/simplesocks/Socks5Server.java
+++ simplesocks/src/main/java/org/connectbot/simplesocks/Socks5Server.java
@@ -14,9 +14,14 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package org.connectbot.simplesocks;
 
-import java.io.*;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.InetAddress;
 import java.nio.ByteBuffer;
 import java.nio.CharBuffer;
